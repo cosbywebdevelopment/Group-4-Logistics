98365453b435a4bb65ad43bccb39aaae334a6c83
cosbywebdevelopment <nicknexus1973@gmail.com>
2022-03-28 18:14:15 +0100

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -108,7 +108,7 @@
           </div>
         </div>
         <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
-          <img src="assets/img/van.png" class="img-fluid" alt="">
+          <img src="/assets/img/van.png" class="img-fluid" alt="">
         </div>
       </div>
     </div>
