ea51b84f620913aa9e89085d84efe39325d29d0c
cosbywebdevelopment <nicknexus1973@gmail.com>
2022-04-24 12:01:52 +0100

    Update index.blade.php

--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -120,7 +120,7 @@
                     </div>
                     <div class="col-md-6">
                         <div class="info-box text-center">
-                            <h3><i class="bi bi-envelope align-middle"></i><span class="pl-3">Package</span></h3>
+                            <h3><i class="bi bi-envelope align-middle"></i><span class="pl-3">Package/Pallets</span></h3>
                             <p class="font-weight-bold">{{ $pallet }}</p>
                         </div>
                     </div>
