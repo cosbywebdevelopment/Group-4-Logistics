a3d85d4c7aab46c0a739655c1bdf0c090f2c522d
cosbywebdevelopment <nicknexus1973@gmail.com>
2022-04-25 15:29:06 +0100

    Orders

---
+++ database/migrations/2022_04_25_141914_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('type');
+            $table->string('pickup');
+            $table->string('drop_off');
+            $table->string('time');
+            $table->string('date');
+            $table->string('package');
+            $table->string('mileage');
+            $table->string('cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
