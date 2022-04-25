dc1b48712fa3683d56a790cc8ff222da650877b4
cosbywebdevelopment <nicknexus1973@gmail.com>
2022-04-25 16:20:52 +0100

    removed checkout from nav

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -89,8 +89,9 @@
             </ul>
           </li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="nav-link scrollto" href="/checkout">Checkout</a>
-          </li>
+{{--          <li>--}}
+{{--              <a class="nav-link scrollto" href="/checkout">Checkout</a>--}}
+{{--          </li>--}}
           <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
