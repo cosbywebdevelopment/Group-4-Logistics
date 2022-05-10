bbe40eab1f6ce36c2570ecf5d95235217173059a
cosbywebdevelopment <nicknexus1973@gmail.com>
2022-05-10 09:54:12 +0100

    Update app.blade.php

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ appName() }} | @yield('title')</title>
-    <meta name="description" content="@yield('meta_description', appName())">
-    <meta name="author" content="@yield('meta_author', 'Anthony Rappa')">
+{{--    <meta name="description" content="@yield('meta_description', appName())">--}}
+{{--    <meta name="author" content="@yield('meta_author', 'Anthony Rappa')">--}}
     @yield('meta')
+    <link href="/assets/img/favicon.ico" rel="icon">
+    <link href="/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     @stack('before-styles')
     <link href="{{ mix('css/backend.css') }}" rel="stylesheet">
