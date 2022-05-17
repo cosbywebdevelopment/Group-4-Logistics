83373fe642ada8e06a55f9ccf135b357c338adde
cosbywebdevelopment <nicknexus1973@gmail.com>
2022-05-17 12:51:23 +0100

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1767,23 +1767,6 @@
 
         };
 
-    // const swiper = new Swiper('.swiper', {
-    //     // Optional parameters
-    //     direction: 'horizontal',
-    //     loop: true,
-    //
-    //     // If we need pagination
-    //     // pagination: {
-    //     //     el: '.swiper-pagination',
-    //     // },
-    //
-    //     // Navigation arrows
-    //     navigation: {
-    //         nextEl: '.swiper-button-next',
-    //         prevEl: '.swiper-button-prev',
-    //     },
-    //
-    // });
 </script>
 
 <script async
